eceada8a182d185f2d7272611e2c7987645e00b5
pkh706 <111472676+pkh706@users.noreply.github.com>
2022-10-26 10:59:36 +0900

    Add files via upload
    221026

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+	int score[6];
+	
+	score[0] = 10;
+	
+	
+	char alpha[50];
+	
+	char str[6];
+}
---
+++ Untitled2.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+main()
+{
+	/*int score[6];
+	
+	int score[0] = 10;
+	int score[1] = 33;
+	int score[2] = 50;
+	int score[3] = 88;
+	int score[4] = 90;
+	int score[5] = 100;
+	
+	char alpha[50];
+	char str[6];
+	
+	int arr1[5] = {1, 2, 3};
+	
+	int i; 
+	
+	int num[3];
+	
+	num[0] = 10;
+	num[1] = 20;
+	num[2] = 30;
+	
+	for(i = 0 ; i < 3 ; i++)
+	{
+		printf("num[%d] = %d\n", i, num[i]);
+		
+	int arr[5] = {10, 15, 85, 66, 20};
+	printf("%d\n", arr[0]);
+	printf("%d\n", arr[2]);
+	printf("%d\n", arr[4]);*/
+	
+	int a[5], i;
+	
+	for(i = 0; i<5; i++)
+	{
+		a[5] = 5;
+	}
+	
+	printf("a[5] : %d\n", a[5]);
+	printf("a[7] : %d\n", a[7]);
+}
